336db2757cdbe3e2083e97b547810a0491890250
ClinicRoomComputer <57233862+ellieSundheim@users.noreply.github.com>
2025-04-11 16:09:50 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='qickquack',
+    version='0.0.1',
+    author='HMC HRL Clinic Team 24-25',
+    description= 'An extension for qick to use custom DACs',
+    url= 'https://github.com/hrl-labs-clinic-24-25/qickquack'
+
+)
