d5b63f7949a1241fd1b307097ea7337c96014eb7
ClinicRoomComputer <57233862+ellieSundheim@users.noreply.github.com>
2025-05-01 11:15:49 -0700

    reformatting files
    i.e. copying qick's structure shamelessly

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='qickquack',
-    version='0.0.1',
+    version='0.0.2',
     author='HMC HRL Clinic Team 24-25',
     description= 'An extension for qick to use custom DACs',
     url= 'https://github.com/hrl-labs-clinic-24-25/qickquack',
     install_requires = [
         'qick'
-    ]
+    ],
+    package_dir = {'': 'qickquack_lib'},
+    packages = find_namespace_packages(where='qickquack_lib'),
+    package_data = {'qickquack': 
+                    ['*.bit', '*.hwh']}
 
 )
